95adccdc9df8132281c88df7441692329efe9fd8
Cole Boggan (RIT Student) <cxb6159@ad.rit.edu>
2019-01-26 15:21:41 -0500

    Working on saving the stuffs
    added a saving script which utilizes Unity's PlayerPrefs to save data for the game.

---
+++ HangYourHat/Hang Your Hat/Assets/Scripts/Saving.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saving : MonoBehaviour {
+    public GameObject characterData;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SaveCharacter(characterData.GetComponent<Player>(), 1);//characterData, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+
+        }
+	}
+
+    static void SaveCharacter(Player toSave, int characterSlot)
+    {
+        PlayerPrefs.SetInt("health_CharacterSlot" + characterSlot, toSave.currentHealth)
+    }
+
+    void SetSaveChar(GameObject _character)
+    {
+        characterData = _character;
+    }
+}
