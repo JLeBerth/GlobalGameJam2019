d578bfaeddfd7824aeaf61508a398791277794d4
Jonathan Sands (RIT Student) <jxs4264@ad.rit.edu>
2019-01-25 23:41:13 -0500

    Added rolling and tried to fix falling
    Added rolling and tried to fix falling.  Currently doesn't work.  Trying out raycasts.  Will try tomorrow

--- HangYourHat/Hang Your Hat/Assets/Scripts/Player.cs
+++ HangYourHat/Hang Your Hat/Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myBody;              //players rigidBody
+    public Collision2D botCollision;                  // Stores a vertical collision
+    public Collision2D test = new Collision2D();
     public Vector2 position;                //the players current location
     public Vector3 velocity;                //the players velocity added to position
     public Vector3 acceleration;            //the players acceleration added to velocity
@@ -14,8 +16,12 @@ public class Player : MonoBehaviour
     public float mass;                      //the mass of a player
     public float maxAcceleration;             //the maximum acceleration of a player
     public float maxVelocity;                 //the maximum speed of a player
+    public float rollSpeed;                 // This is the speed of a player while rolling.  Higher than regular.
+    public float normalSpeed;               // This is the normal speed
     public float fallSpeed;                 //rate of downards acceleration
     public float jumpSpeed;                 //jump height
+    public float timer;
+    public float distToGround;              //Distance from the center of the sprite to the ground
 
 
     public int baseHealth;                  //the total amount of health the player has
@@ -26,12 +32,14 @@ public class Player : MonoBehaviour
     public double bulletsTillReload;        //the number of times the player may shoot before reloading
 
     public bool falling;                    //boolean of whether the player is currently falling
+    public bool rolling;                    // Boolean of whether the player is currently rolling
 
 	// Use this for initialization
 	void Start ()
     {
         position = transform.position;
         myBody = gameObject.GetComponent<Rigidbody2D>();
+        distToGround = gameObject.GetComponent<SpriteRenderer>().bounds.extents.y;
 	}
 	
 	// Update is called once per frame
@@ -50,39 +58,95 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             ApplyForce(new Vector2(maxVelocity, 0));
+
+            if (Input.GetKey(KeyCode.Space) && timer > .2f)
+            {
+                rolling = true;
+                timer = 0;
+                
+            }
         }
 
         // moving left
         if (Input.GetKey(KeyCode.A))
         {
             ApplyForce(new Vector2(-maxVelocity, 0));
+
+            if (Input.GetKey(KeyCode.Space) && timer > .2f)
+            {
+                rolling = true;
+                timer = 0;
+
+            }
         }
 
         //jump
-        if (Input.GetKey(KeyCode.W) && !falling)
+        if (Input.GetKey(KeyCode.W) && 
+            !falling
+            //IsGrounded()
+            )
         {
             ApplyForce(new Vector2(0, jumpSpeed));
             falling = true;
         }
+        
+        if (IsGrounded())
+        {
+            falling = false;
+        }
 
+        // Falling
         if(falling)
         {
             ApplyForce(new Vector2(0, fallSpeed));
         }
         
-        
-        Movement();
+        // Roll
+        if (rolling)
+        {
+            maxVelocity = rollSpeed;
+        }
+        else
+        {
+            maxVelocity = normalSpeed;
+        }
+
+        // Timer for roll
+        timer += .01f;
+        if (rolling && timer >= .1f)
+        {
+            rolling = false;
+        }
+
+        // if rolling, then roll
+        if (rolling)
+        {
+            Roll();
+        }
+        // Otherwise, move as normal
+        else
+        {
+            Movement();
+        }
 
         velocity.x = velocity.x / 2;
 
 
-        
 
+        // falling = true;
         //move player after calculations
         
 
     }
 
+    /// <summary>
+    /// Method to check if player is grounded??? Doesn't work, but it might
+    /// </summary>
+    /// <returns></returns>
+    public bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector2.up, distToGround + .1f);
+    }
     /// <summary>
     /// AppkyForce Method accelerates based on force
     /// </summary>
@@ -110,6 +174,24 @@ public class Player : MonoBehaviour
         myBody.MovePosition(transform.position + velocity * Time.deltaTime);
     }
 
+    /// <summary>
+    /// This executes a roll instead of normal movement
+    /// </summary>
+    public void Roll()
+    {
+        if (velocity.x > 0)
+        {
+            velocity.x = rollSpeed;
+        }
+
+        else if (velocity.x < 0)
+        {
+            velocity.x = -rollSpeed;
+        }
+
+        myBody.MovePosition(transform.position + velocity * Time.deltaTime);
+    }
+
 
     /// <summary>
     /// Player Death Method called when the player reaches zero or less health, inflicts the death penalty.
@@ -132,12 +214,13 @@ public class Player : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        falling = false;
-        velocity.y = 0;
+        if (falling)
+        {
+            falling = false;
+            velocity.y = 0;
+            botCollision = collision;
+        }
     }
+    
 
-    public void OnCollisionExit2D(Collision2D collision)
-    {
-        falling = true;
-    }
 }
