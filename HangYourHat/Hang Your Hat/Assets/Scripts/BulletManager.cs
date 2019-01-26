f44a69b37c9e2f42b05a56d1d6af43e06529e831
Jonathan Sands (RIT Student) <jxs4264@ad.rit.edu>
2019-01-26 12:49:35 -0500

    Fixed movement.
    Fixed movement so it's all working and also started bullet functionality

---
+++ HangYourHat/Hang Your Hat/Assets/Scripts/BulletManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BulletManager : MonoBehaviour
+{
+    public static List<GameObject> bullets;
+    public float speed = 2;
+
+
+    void Awake()
+    {
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        bullets = new List<GameObject>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (bullets.Count > 0)
+        {
+            foreach (GameObject b in bullets)
+            {
+                b.transform.Translate(speed * b.transform.forward);
+            }
+        }
+    }
+}
