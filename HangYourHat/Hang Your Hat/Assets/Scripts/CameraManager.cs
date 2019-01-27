e0621f9268efd3f67b4946ae30b9728ac76e07b1
Jonathan Sands (RIT Student) <jxs4264@ad.rit.edu>
2019-01-26 22:07:55 -0500

    Added a semi-functioning camera

---
+++ HangYourHat/Hang Your Hat/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public float width;
+    public float position;
+    public GameObject player;
+    public float speed;
+    public Vector3 camSpeed;
+
+    float maxPosition;
+    float minPosition;
+	// Use this for initialization
+	void Start ()
+    {
+        camSpeed = new Vector3(speed, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        maxPosition = transform.position.x - position + width;
+        minPosition = transform.position.x - position;
+
+        if( player.transform.position.x > maxPosition )
+        {
+            transform.position += camSpeed;
+        }
+        if (player.transform.position.x < minPosition)
+        {
+            transform.position -= camSpeed;
+        }
+	}
+}
