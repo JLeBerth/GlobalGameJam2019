9eefd48a2e890fe25effa08843fd45c681f3bd9e
Cole Boggan (RIT Student) <cxb6159@ad.rit.edu>
2019-03-05 16:39:14 -0500

    Edits to orca prefab, and enimy code
    added enemy raycasting abilities to dtect walls and ledges to avoid walking off of them. However, currently falling code doesn't allow enemy to move for unknown reasons.

--- HangYourHat/Hang Your Hat/Assets/Scripts/Enemy.cs
+++ HangYourHat/Hang Your Hat/Assets/Scripts/Enemy.cs
@@ -10,11 +10,14 @@ public class Enemy : MonoBehaviour {
     public Vector3 acceleration;
     public Vector3 direction;
     public Vector3 startPos;
+    public Vector2 tempPosition;         // temporary position for raycasting the ground
 
     // Objects
     private Player playerScript;    // Holds the player script for reference
     public GameObject playerObject; // Holds the player object
     public Rigidbody2D enemyRigid;  // Holds the enemy rigidBody
+    public SpriteRenderer sprite;   // Holds the enemy sprite
+    public RaycastHit2D ground;
     public GameObject pivotPoint;   // Holds the pivotPoint for the gun
     public GameObject bulletSpawn;  // Where the bullet spawns
     public GameObject gun;            // the gun
@@ -26,6 +29,7 @@ public class Enemy : MonoBehaviour {
 
     public float enemySpeed;        // Max speed that the enemy moves
     public float mass;
+    public float fallSpeed = -1.2f;
 
     public float maxBullets;
     public float currentBullets;
@@ -42,6 +46,8 @@ public class Enemy : MonoBehaviour {
     public bool bShoot;              // Should the enemy shoot at the player?
     private bool moveRight;
     private bool moveLeft;
+    public bool falling;
+
     public bool backToStart;
     public bool alive;
 
@@ -62,6 +68,7 @@ public class Enemy : MonoBehaviour {
         //playerScript = playerObject.GetComponent<Player>();
 
         enemyRigid = this.GetComponent<Rigidbody2D>();
+        sprite = this.GetComponent<SpriteRenderer>();
         startPos = this.transform.position;
 
         currentShootTimer = 0;
@@ -71,7 +78,7 @@ public class Enemy : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         // Move the enemy
-        velocity += acceleration * Time.deltaTime;
+        /*velocity += acceleration * Time.deltaTime;
 
         velocity = Vector3.ClampMagnitude(velocity, enemySpeed);
 
@@ -81,7 +88,32 @@ public class Enemy : MonoBehaviour {
 
         acceleration = Vector3.zero; // zero out acceleration so each frame the forces are taken into effect
 
-        enemyRigid.position = enemyPosition;
+        enemyRigid.position = enemyPosition;*/
+
+        tempPosition = transform.position;
+        tempPosition.y -= sprite.bounds.extents.y;
+
+        ground = Physics2D.Raycast(tempPosition, -Vector2.up);
+
+        velocity += acceleration;
+        if (velocity.x > enemySpeed)
+        {
+            velocity.x = enemySpeed;
+        }
+        else if (velocity.x < -enemySpeed)
+        {
+            velocity.x = -enemySpeed;
+        }
+
+        if (falling)
+        {
+            if (transform.position.y + velocity.y < ground.point.y + .1f)
+            {
+                enemyRigid.MovePosition(new Vector2(transform.position.x, ground.point.y + .1f));
+            }
+        }
+
+        enemyRigid.MovePosition(transform.position + velocity * Time.deltaTime);
 
         // Check player distance and associated fxns
 	}
@@ -162,7 +194,9 @@ public class Enemy : MonoBehaviour {
     }
 
     /// <summary>
-    /// Checks if the player is close enough, and wanders if they are
+    /// Checks if the player is close enough, and wanders if they are.
+    /// Uses raycasting to detect whether there is a wall in front of them or the gorund ends, in which case they 
+    /// will turn around.
     /// </summary>
     public void Wander()
     {
@@ -178,7 +212,85 @@ public class Enemy : MonoBehaviour {
 
         if (bWander && !bShoot) // If the player is close enough, do below code
         {
-            if (enemyPosition.x >= startPos.x + 2f)
+            LayerMask mask = LayerMask.GetMask("Collision");
+
+            // Left Ray
+            tempPosition = transform.position;
+
+            tempPosition.x -= sprite.bounds.extents.x - .5f;
+            //tempPosition.y -= sprite.bounds.extents.y;
+
+            Debug.DrawLine(tempPosition, tempPosition + new Vector2(0, -1) * sprite.bounds.extents.y, Color.black);
+
+            RaycastHit2D hit1 = Physics2D.Raycast(tempPosition, -Vector2.up, sprite.bounds.extents.y); // starting from tempPosition, cast a ray down
+
+
+            // Right Ray
+            tempPosition = transform.position;
+
+            tempPosition.x += sprite.bounds.extents.x - .5f;
+            //tempPosition.y -= sprite.bounds.extents.y;
+
+            Debug.DrawLine(tempPosition, tempPosition + new Vector2(0, -1) * sprite.bounds.extents.y, Color.blue);
+
+            RaycastHit2D hit2 = Physics2D.Raycast(tempPosition, -Vector2.up, sprite.bounds.extents.y); // start from tempPosition, cast ray down
+
+
+            // Conditionals
+            if (hit1.collider == null && hit2.collider == null) // check if enemy should fall
+            {
+                Debug.Log("FAAAAALL");
+                ApplyForce(new Vector2(0f, fallSpeed));
+            }
+
+            if (backToStart) // left
+            {
+                ApplyForce(new Vector3(-enemySpeed, 0f, 0f)); // Move the enemy left
+
+                // Check the down raycast to see if there is no longer a floor beneath the enemy on the left side
+                if ((hit2.distance < hit1.distance && hit2.collider != null) || (hit1.collider == null && hit2.collider != null))
+                {
+                    backToStart = false; // if so, turn around
+                }
+
+                // Read for wall to the left
+                tempPosition.x = transform.position.x - (sprite.bounds.extents.x - .7f); // reset temp pos
+
+                RaycastHit2D forward = Physics2D.Raycast(tempPosition, Vector2.left, .5f); // cast ray left
+
+                Debug.DrawLine(tempPosition, tempPosition + Vector2.left * 1f, Color.black);
+
+                if (forward.collider != null) // if the ray hits something, turn around
+                {
+                    velocity = Vector2.zero;
+                    backToStart = false;
+                }
+            }
+            else if (!backToStart) // right
+            {
+                ApplyForce(new Vector3(enemySpeed, 0f, 0f));
+
+                // Check the down raycasts to see if there is no longer a floor beneath the enemy on the right
+                if ((hit1.distance < hit2.distance && hit1.collider != null) || (hit2.collider == null && hit1.collider != null))
+                {
+                    backToStart = true; // if so, turn around
+                }
+
+                // Read for wall to the right
+                tempPosition.x = transform.position.x + (sprite.bounds.extents.x - .7f); // reset temp pos
+
+                RaycastHit2D forward = Physics2D.Raycast(tempPosition, Vector2.right, .5f); // cast ray right
+
+                Debug.DrawLine(tempPosition, tempPosition + Vector2.right * 1f, Color.blue);
+
+                if (forward.collider != null) // if the ray hits, turn around
+                {
+                    velocity = Vector2.zero;
+                    backToStart = true;
+                }
+            }
+
+            /*if (enemyPosition.x >= startPos.x + 2f)
             {
                 backToStart = true;
             }
@@ -198,12 +310,14 @@ public class Enemy : MonoBehaviour {
             {
                 ApplyForce(new Vector3(2f, 0f, 0f));
                 //Debug.Log("right");
-            }
+            }*/
         }
         else
         {
             velocity = Vector3.zero;
         }
+
+        
     }
 
     public void TakeDamage()
