36a8d89a3abbd8b8cee717af15c2f1c4a049d786
Jacob Leberth (RIT Student) <jtl4027@ad.rit.edu>
2019-03-19 15:34:29 -0400

    Work on toolset

---
+++ HangYourHat/Hang Your Hat/Assets/Editor/TileMapEditor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TileMap))]
+public class TileMapEditor : Editor
+{
+
+    public TileMap map;
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.BeginVertical();
+        map.mapSize = EditorGUILayout.Vector2Field("Map Size", map.mapSize);
+        map.texture2D = (Texture2D)EditorGUILayout.ObjectField("Texture2D", map.texture2D, typeof(Texture2D), false);
+        if(map.texture2D==null)
+        {
+            EditorGUILayout.HelpBox("No Texture has been selected yet", MessageType.Warning);
+        }
+        else
+        {
+            EditorGUILayout.LabelField("Tile Size:", map.tileSize.x + "x" + map.tileSize.y);
+            EditorGUILayout.LabelField("Grid Size In Units", map.gridSize.x + "x" + map.gridSize.y);
+            EditorGUILayout.LabelField("Pixels To Units:", map.pixelstToUnits.ToString());
+        }
+        EditorGUILayout.EndVertical();
+    }
+
+    private void OnEnable()
+    {
+        map = target as TileMap;
+        Tools.current = Tool.View;
+        if(map.texture2D !=null)
+        {
+            var path = AssetDatabase.GetAssetPath(map.texture2D);
+            map.spriteReferences = AssetDatabase.LoadAllAssetsAtPath(path);
+
+            var sprite = (Sprite)map.spriteReferences[1];
+            var width = sprite.textureRect.width;
+            var height = sprite.textureRect.height;
+
+            map.tileSize = new Vector2(width, height);
+
+            map.pixelstToUnits = (int)(sprite.rect.width / sprite.bounds.size.x);
+
+            map.gridSize = new Vector2((width / map.pixelstToUnits) * map.mapSize.x, (height / map.pixelstToUnits) * map.mapSize.y);
+        }
+    }
+}
---
+++ HangYourHat/Hang Your Hat/Assets/Scripts/TileMap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour
+{
+    public Vector2 mapSize = new Vector2(20, 10);
+    public Texture2D texture2D;
+    public Vector2 tileSize = new Vector2();
+    public Object[] spriteReferences;
+    public Vector2 gridSize = new Vector2();
+    public int pixelstToUnits = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        var pos = transform.position;
+
+        if(texture2D != null)
+        {
+            Gizmos.color = Color.white;
+            var centerX = pos.x + (gridSize.x / 2);
+            var centerY = pos.y - (gridSize.y / 2);
+
+            Gizmos.DrawWireCube(new Vector2(centerX, centerY), gridSize);
+        }
+    }
+}
